145ee5b745920fe297ec84f597755cc7b6f227dd
miron <mironoprea165@gmail.com>
2020-05-08 00:41:32 +0300

    `import tkinter

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #! Python3
 # Compound-Interest-Calculator Calc
 
-import tkinter
+initial_deposit = input('Please input initial deposit: ')
+annual_deposit = input('Please input regular yearly deposit , if any: ')
+interest_rate = input('Please input yearly interest rate: ')
+period = input('Please input time in years: ')
 
 
-#TODO: GUI
+
+
+
+
+
